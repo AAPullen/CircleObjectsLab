c37dc4e349c5cd512570c3c3d14c0ded4e3d05ca
Andrew <andrewpullen1@gmail.com>
2023-02-09 19:19:02 -0500

    init commit

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    public class Circle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public static double CalculateCircumference(Circle userCircle)
+        {
+
+            double userCircumference = 2 * Math.PI * userCircle.radius;
+            userCircumference = Math.Round(userCircumference, 2);
+            return userCircumference;
+        }
+
+        public static double CalculateArea(Circle userCircle)
+        {
+            double userArea = Math.PI * (Math.Pow(userCircle.radius, 2));
+            userArea = Math.Round(userArea, 2);
+            return userArea;
+        }
+
+        public static string CalculateFormattedCircumference(double userCircumference)
+        {
+            string circumferenceString = userCircumference.ToString();
+            return circumferenceString;
+        }
+
+        public static string CalculateFormattedArea(double userArea)
+        {
+            string areaString = userArea.ToString();
+            return areaString;
+        }
+
+        // The following methods demonstrate ways to access or manipulate private methods and variables
+
+        private static string FormatNumber(double x)
+        {
+            string xString = x.ToString();
+            return xString;
+        }
+
+        public static string PublicFormatNunmber(double x)
+        {
+            string xString = FormatNumber(x);
+            return xString;
+        }
+
+        public static Circle ChangeRadius(Circle userCircle, double newRadius)
+        {
+            userCircle.radius = newRadius;
+            return userCircle;
+        }
+
+        public static string PrintRadius(Circle userCircle)
+        {
+
+            return userCircle.radius.ToString();
+        }
+
+    }
+}
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,50 @@
+﻿using CircleObjectsLab;
+
+string loop;
+int circlesCreated = 0;
+
+Console.WriteLine("Welcome to the Circle Tester\n");
+
+do
+{
+	loop = null;
+	double userRadius = 0;
+    bool isValidRadius = false;
+
+	while (isValidRadius == false)
+	{
+		Console.Write("Enter a radius: ");
+		try
+		{
+			userRadius = double.Parse(Console.ReadLine());
+			isValidRadius = true;
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("\nI'm sorry, that is not a valid input.\n");
+		}
+	}
+
+	Circle userCircle = new Circle(userRadius);
+
+	double userCircumference = Circle.CalculateCircumference(userCircle);
+	string circumferenceString = Circle.CalculateFormattedCircumference(userCircumference);
+	Console.WriteLine($"Circumference: {circumferenceString}");
+
+	double userArea = Circle.CalculateArea(userCircle);
+	string areaString = Circle.CalculateFormattedArea(userArea);
+	Console.WriteLine($"Area: {areaString}");
+
+	while (loop != "y" && loop != "n")
+	{
+		Console.Write("Continue? (y/n): ");
+		loop = Console.ReadLine().ToLower();
+		Console.WriteLine();
+	}
+
+	circlesCreated++;
+
+
+} while (loop == "y");
+
+Console.WriteLine($"Goodbye. You created {circlesCreated} Circle object(s).");
